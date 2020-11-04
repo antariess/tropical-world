d5c71806b48ee8ab108b7c6cc7e6af655432abe3
xav <xavier.bruggen@northcoders.com>
2020-11-04 12:49:15 +0000

    service layer interface demo
    adding service/data layers and referencing service from api, data layer is not registered and mongo driver nuget not installed yet

---
+++ Service/NameService/AnotherNameService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Service
+{
+    public class AnotherNameService
+    {
+        public AnotherNameService()
+        {
+        }
+    }
+}
---
+++ Service/NameService/INameService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Service
+{
+    public interface INameService
+    {
+    }
+}
