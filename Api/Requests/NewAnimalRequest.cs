45c378538f107b672408b88eeae8f9c2303259f4
xav <xavier.bruggen@northcoders.com>
2020-11-05 17:04:41 +0000

    made name field required in NewAnimalRequest

--- Api/Requests/NewAnimalRequest.cs
+++ Api/Requests/NewAnimalRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Api.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Requests
 {
     public class NewAnimalRequest
     {
+        [Required]
         public string Name { get; set; }
     }
 }
